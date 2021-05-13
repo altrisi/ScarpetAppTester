339fc9dc3d5858d4bb047c82ab6b7fa4cdd53dfc
altrisi <altrisi.trillosierra@gmail.com>
2021-05-13 19:49:57 +0200

    LoadingTest results and elapsed time
    Resolves #8 for now

--- src/main/java/altrisi/scarpetapptester/testing/tests/LoadingTest.java
+++ src/main/java/altrisi/scarpetapptester/testing/tests/LoadingTest.java
@@ -1,5 +1,7 @@
 package altrisi.scarpetapptester.testing.tests;
 
+import java.util.Map;
+
 import altrisi.scarpetapptester.AppTester;
 import altrisi.scarpetapptester.ScarpetAppTester;
 import altrisi.scarpetapptester.testing.apps.App;
@@ -7,8 +9,9 @@ import carpet.CarpetServer;
 import carpet.script.CarpetScriptHost;
 
 public class LoadingTest extends AbstractTest {
-	private boolean loaded;
+	private boolean success;
 	public CarpetScriptHost resultHost;
+	private long elapsedTime;
 
 	public LoadingTest(App app) {
 		super(app, "App Loading");
@@ -16,7 +19,7 @@ public class LoadingTest extends AbstractTest {
 
 	@Override
 	public void finishTest() {
-		if (loaded) {
+		if (success) {
 			resultHost = (CarpetScriptHost) CarpetServer.scriptServer.getHostByName(app.name());
 		} else {
 			AppTester.LOGGER.fatal("Failed to load app " + app);
@@ -25,12 +28,15 @@ public class LoadingTest extends AbstractTest {
 
 	@Override
 	public boolean successfulSoFar() {
-		return loaded;
+		return success;
 	}
 
 	@Override
 	public void runTests() {
-		loaded = CarpetServer.scriptServer.addScriptHost(ScarpetAppTester.commandSource, app.name(), null, true, false, false);
+		long start = System.nanoTime();
+		success = CarpetServer.scriptServer.addScriptHost(ScarpetAppTester.commandSource, app.name(), null, true, false, false);
+		long end = System.nanoTime();
+		elapsedTime = (end-start)/1000000;
 	}
 
 	@Override
@@ -41,6 +47,14 @@ public class LoadingTest extends AbstractTest {
 
 	@Override
 	public TestResults getResults() {
-		return () -> !loaded ? "App failed to load" : "App got loaded in ";//+todo time;
+		return new LoadResult(success, elapsedTime);
+	}
+	
+	public record LoadResult(boolean success, long elapsedTime) implements TestResults {
+		@Override
+		public Map<String, String> getResultsMap() {
+			return Map.of("App load", success ? "Correct" : "Failed", "Elapsed time", elapsedTime + "ms");
+		}
+		
 	}
 }
--- src/main/java/altrisi/scarpetapptester/testing/tests/TestResults.java
+++ src/main/java/altrisi/scarpetapptester/testing/tests/TestResults.java
@@ -1,7 +1,20 @@
 package altrisi.scarpetapptester.testing.tests;
 
-@FunctionalInterface
+import java.util.Map;
+import java.util.Map.Entry;
+
 public interface TestResults {
 
-	String toPrettyString();
+	Map<String, String> getResultsMap();
+	
+	default String toPrettyString() {
+		StringBuilder string = new StringBuilder();
+		for (Entry<String, String> e : getResultsMap().entrySet()) {
+			string.append(e.getKey());
+			string.append(": ");
+			string.append(e.getValue());
+			string.append('\n');
+		}
+		return string.toString();
+	}
 }
